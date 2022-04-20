292b9bd3ddca955aaed3288a83be846356ced7ea
Mehtab Ahmed <work4edu@gmail.com>
2022-04-20 15:43:08 +0100

    Remote API class and endPoints added to fetch and parse data from APIs

---
+++ LBCodeChallenge/Infrastructure/Remote/EndPoints.swift
@@ -0,0 +1,14 @@
+//
+//  EndPoints.swift
+//  LBCodeChallenge
+//
+//  Created by Mehtab Ahmed on 20/04/2022.
+//
+
+import Foundation
+
+struct EndPoints {
+   
+    static let animalsList = "https://zoo-animal-api.herokuapp.com/animals/rand/10"
+    static let usersList = "https://run.mocky.io/v3/bf1cf35a-81ac-4801-b67a-c071537b411c"
+}
---
+++ LBCodeChallenge/Infrastructure/Remote/NetworkService.swift
@@ -0,0 +1,139 @@
+//
+//  RemoteAPI.swift
+//  LBCodeChallenge
+//
+//  Created by Mehtab Ahmed on 20/04/2022.
+//
+
+import Foundation
+import Alamofire
+import UIKit
+import SwiftyJSON
+
+enum Errors: String {
+    
+    case NETWORK_ERROR, SERIALIZATION_ERROR, OTHER
+    
+    var description: String {
+        switch self {
+        case .NETWORK_ERROR:
+            return "No internet connection!"
+        case .SERIALIZATION_ERROR:
+            return "Cannot parse data. Data is not in the correct format"
+        case .OTHER:
+            return "Something went wrong"
+        }
+    }
+}
+
+
+class RemoteAPI {
+    
+    static let shared = RemoteAPI()
+    
+    func getRequest<T:Decodable>(url: String, parameters: [String : AnyObject] = [:], headers: [String : String] = [:], completion: @escaping (T) -> Void, failure: @escaping (String) -> Void ) {
+        
+        Alamofire.request(url, method: .get , parameters: parameters, encoding: URLEncoding.default, headers: headers).responseJSON { response in
+            self.handleResponse(response: response, completion: completion, failure: failure)
+        }
+    }
+    
+    
+    func handleResponse<T:Decodable>(response: DataResponse<Any>, completion: @escaping (T) -> Void, failure: @escaping (String) -> Void) {
+        
+        switch response.result {
+        case .success:
+            self.showRequestDetailForSuccess(responseObject: response)
+            parseJSON(json: JSON(response.result.value!), completion: completion, failure: failure)
+            break
+            
+        case .failure:
+            self.showRequestDetailForFailure(responseObject: response)
+            failure(response.result.error?.localizedDescription ?? "Server request failed")
+            break
+        }
+    }
+    
+    
+    func parseJSON<T:Decodable>(json:JSON, completion: @escaping (T) -> Void, failure: @escaping (String) -> Void) {
+        
+        do {
+            let data = try json.rawData()
+            let parsedData = try JSONDecoder().decode(T.self, from: data)
+            completion(parsedData)
+        }
+        
+        catch DecodingError.keyNotFound(let key, let context) {
+            Swift.print("could not find key \(key) in JSON: \(context.debugDescription)")
+            failure(Errors.SERIALIZATION_ERROR.rawValue)
+        } catch DecodingError.valueNotFound(let type, let context) {
+            Swift.print("could not find type \(type) in JSON: \(context.debugDescription)")
+            failure(Errors.SERIALIZATION_ERROR.rawValue)
+        } catch DecodingError.typeMismatch(let type, let context) {
+            Swift.print("type mismatch for type \(type) in JSON: \(context.debugDescription)")
+            failure(Errors.SERIALIZATION_ERROR.rawValue)
+        } catch DecodingError.dataCorrupted(let context) {
+            Swift.print("data found to be corrupted in JSON: \(context.debugDescription)")
+            failure(Errors.SERIALIZATION_ERROR.rawValue)
+        } catch let error as NSError {
+            NSLog("Error in read(from:ofType:) domain= \(error.domain), description= \(error.localizedDescription)")
+            failure(Errors.SERIALIZATION_ERROR.rawValue)
+        }
+    }
+    
+    
+    func showRequestDetailForSuccess(responseObject response : DataResponse<Any>) {
+        
+        print("\n\n\n ✅ ✅ ✅ ✅ ✅  ------- Success Response Start -------  ✅ ✅ ✅ ✅ ✅ \n")
+        print(""+(response.request?.url?.absoluteString ?? ""))
+        print("\n=========   allHTTPHeaderFields   ========== \n")
+        print("%@",response.request!.allHTTPHeaderFields!)
+        
+        if let bodyData : Data = response.request?.httpBody
+        {
+            let bodyString = String(data: bodyData, encoding: String.Encoding.utf8)
+            print("\n=========   Request httpBody   ========== \n" + bodyString!)
+            
+        } else {
+            print("\n=========   Request httpBody   ========== \n" + "Found Request Body Nil")
+        }
+        
+        if let responseData : Data = response.data {
+            let responseString = String(data: responseData, encoding: String.Encoding.utf8)
+            print("\n=========   Response Body   ========== \n" + responseString!)
+            
+        } else {
+            print("\n=========   Response Body   ========== \n" + "Found Response Body Nil")
+        }
+        
+        print("\n\n\n ✅ ✅ ✅  ------- Success Response End -------  ✅ ✅ ✅ \n\n\n")
+    }
+    
+    
+    func showRequestDetailForFailure(responseObject response : DataResponse<Any>) {
+        
+        print("\n\n\n ❌ ❌ ❌ ❌ ❌ ------- Failure Response Start ------- ❌ ❌ ❌ ❌ ❌ \n")
+        print(""+(response.request?.url?.absoluteString ?? ""))
+        print("\n=========   allHTTPHeaderFields   ========== \n")
+        print("%@",response.request!.allHTTPHeaderFields!)
+        
+        if let bodyData : Data = response.request?.httpBody {
+            let bodyString = String(data: bodyData, encoding: String.Encoding.utf8)
+            print("\n=========   Request httpBody   ========== \n" + bodyString!)
+            
+        } else {
+            print("\n=========   Request httpBody   ========== \n" + "Found Request Body Nil")
+        }
+        
+        if let responseData : Data = response.data {
+            let responseString = String(data: responseData, encoding: String.Encoding.utf8)
+            print("\n=========   Response Body   ========== \n" + responseString!)
+            
+        } else {
+            print("\n=========   Response Body   ========== \n" + "Found Response Body Nil")
+        }
+        
+        print("\n\n\n ❌ ❌ ❌ ------- Failure Response End ------- ❌ ❌ ❌ \n")
+    }
+}
+
